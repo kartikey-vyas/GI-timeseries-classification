bfdd03ea6cb41e39735003be543344f188b39586
kartikey-vyas <kartikeyvyas23@gmail.com>
2020-03-03 23:12:23 +1300

    initialised with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Understanding spatiotemporal organisation of GI bioelectrical activity at the microscale',
+    author='Kartikey Vyas',
+    license='',
+)
