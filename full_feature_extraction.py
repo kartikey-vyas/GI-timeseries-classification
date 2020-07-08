e872386a448ef59c71ea89cb83a24574dd6f8f44
Kartikey Vyas <kartikeyvyas23@gmail.com>
2020-07-08 19:21:18 +1200

    first hpc script

---
+++ full_feature_extraction.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from tsfresh import extract_features, select_features
+from tsfresh.feature_extraction import MinimalFCParameters
+from tsfresh.utilities.dataframe_functions import impute
+
+df_ach_at = pd.read_hdf("data/processed/ach_at_full.h5")
+
+X = extract_features(df_ach_at, column_id='window_id', column_sort='t', default_fc_parameters=MinimalFCParameters(),
+impute_function=impute)
+y = (df_ach_at[['window_id','y']]
+     .drop_duplicates()
+     .set_index('window_id')
+     .T
+     .squeeze())
+
+X_filtered = select_features(X, y)
+
+X.to_hdf('data/features/achat_full_min.h5', key = 'data', complevel = 9)
+X_filtered.to_hdf('data/features/achat_filtered_min.h5', key = 'data', complevel = 9)
