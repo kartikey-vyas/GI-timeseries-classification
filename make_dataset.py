13a70d80d8f658e7d24a02f6e7132c7b34776cd7
Kartikey Vyas <kartikeyvyas23@gmail.com>
2020-07-09 22:44:13 +1200

    tidied up for 10/07 meeting

---
+++ make_dataset.py
@@ -0,0 +1,8 @@
+# Generate full data set for Ach-AT (all subjects)
+from src.data.load_data import load_MEA_data, label_MEA_data
+
+# load from the Ach-AT folder
+ach_at = load_MEA_data(folder = "data/raw/Ach-AT")
+label_MEA_data(ach_at, 'ach_at_full')
+
+# NEXT SCRIPT -> full_feature_extraction.py
