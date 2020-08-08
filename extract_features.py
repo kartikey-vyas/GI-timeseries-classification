dc0d048e9ba60e8c9f89c79ee7408bb032a856e8
Kartikey Vyas <kartikeyvyas23@gmail.com>
2020-08-08 22:16:14 +1200

    commented and cleaned up repo

--- extract_features.py
+++ extract_features.py
@@ -1,5 +1,26 @@
+""" This script runs parallelized, automated feature extraction on processed MEA data
+        tsfresh is used to extract the set of "Efficient" features that can be computed from the input data
+        It is designed to be used in conjunction with the `extract_features.sl` slurm
+        script, which creates a feature extration job for each signal in the MEA data.
+
+        Command line arguments
+        ----------
+        filename: Name of .h5 file that is placed in the data/processed/ folder.
+                  This should contain the full dataset containing ts signals
+                
+        electrode: Number that represents the electrode number of the signal to use.
+                   Should be set to environment variable $SLURM_ARRAY_TASK_ID
+        
+        Output
+        -------
+        Generates a design matrix with the extracted features for the specified signal.
+        Saves as an hdf (.h5) file in data/features/
+         
+    """
+# IMPORTS
 import argparse
 import logging
+import time
 from datetime import datetime
 import pandas as pd
 from tsfresh import extract_features
@@ -17,7 +38,7 @@ args = parser.parse_args()
 now = datetime.now()
 job_id = now.strftime("%d%m%y_%H%M%S")
 # create logfile
-logging.basicConfig(filename='logs/feature_extraction/feature_extraction_'+args.electrode+'_'+job_id,level=logging.DEBUG)
+logging.basicConfig(filename='logs/feature_extraction/feature_extraction_'+args.electrode+'_'+job_id, level=logging.DEBUG)
 
 if int(args.electrode) not in range(1,60):
     logging.error("Invalid electrode selected - terminating script")
@@ -25,26 +46,34 @@ if int(args.electrode) not in range(1,60):
 
 ## for now, load the full data frame, retrieve the relevant columns and then delete the full df from memory
 ## it would probably be faster to generate 60 dataframes and access the desired one...
-# access complete dataset hdf file
-fpath = "data/processed/" + args.filename
-df_ach_at = pd.read_hdf(fpath)
+
+## LOAD DATA ------------------------------------------------------------------------------------------------
+start = time.process_time()
+
+fpath = "data/processed/"+args.filename
+df = pd.read_hdf(fpath)
 
 logging.info("loaded dataframe")
 
 # select relevant columns
 col = int(args.electrode)
-df = df_ach_at[['t', 'window_id', col]]
+df = df[['t', 'window_id', col]]
 
-# remove full dataset from memory
-df_ach_at = None
-del df_ach_at
-
+## FEATURE EXTRACTION ----------------------------------------------------------------------------------------
 logging.info("beginning feature extraction")
-# begin feature extraction
 X = extract_features(df, column_id='window_id', column_sort='t', default_fc_parameters=EfficientFCParameters(),
 impute_function=impute)
 logging.info("feature extraction complete, ")
 
 # save design matrix
-X.to_hdf('data/features/achat_'+args.electrode+'_eff.h5', key = 'data', complevel = 9)
+X.to_hdf('data/features/achat_'+args.electrode+'_eff.h5', key='data', complevel=9)
 logging.info("features saved to 'data/features/achat_"+args.electrode+"_eff.h5'")
+
+logging.info("time taken = "+str(time.process_time() - start))
+
+# saving potentially useful code here
+# # look for rows in X where len == 1
+# # this is needed due to a quirk in the data processing functions - some of the time windows end up only including 1 step (1ms)
+# idx_to_remove = list(X[X['0__length'] == 1].index)
+# X = X.drop(idx_to_remove)
+# y = y.drop(idx_to_remove)
