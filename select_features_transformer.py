cbe7226ea58ede0d8587c869cdb030cc6ef593d8
Kartikey Vyas <kartikeyvyas23@gmail.com>
2020-09-16 23:30:04 +1200

    fixed some bugs, running on cluster now

--- select_features_transformer.py
+++ select_features_transformer.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import glob
 from src.features.feature_selection import MulticlassFeatureSelector
 
 ## INITIALISE ARGPARSER ---------------------------------------------------------------------------------
@@ -10,12 +11,10 @@ args = parser.parse_args()
 
 X = pd.DataFrame()
 
-for file in glob.glob("../data/features/6000/*"):
+for file in glob.glob("data/features/6000/*"):
     X_n = pd.read_hdf(file)
     X_n.reset_index(0, inplace=True, drop=True)
     X = pd.concat([X, X_n], axis=1)
-
-del X_n
 
 y = pd.read_hdf('data/processed/y_'+str(args.n_classes)+'_class_6000.h5')
 
